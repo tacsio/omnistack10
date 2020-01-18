d12c0c372049a5cc175e9354ba2f6be2f0afdb97
Tarcisio Coutinho <tcs5cin@gmail.com>
2020-01-18 12:21:47 -0300

    fronend web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express();
 
-const connectionUrl = "mongodb://localhost:27017/omnistack";
+//const connectionUrl = "mongodb://localhost:27017/omnistack";
+const connectionUrl = "mongodb://192.168.99.100:27017/omnistack";
+
 mongoose.connect(connectionUrl, {
   useUnifiedTopology: true,
 	useNewUrlParser: true,
 	useCreateIndex: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevFrom from './components/DevForm';
+import DevItem from './components/DevItem'; 
 
 function App() {
+
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(newDev) {
+    const response = await api.post('/devs', newDev);
+    setDevs([...devs, response.data]);
+  }
+  
   return (
     <div>
-      
+      <div id="app">
+        <aside>
+          <strong>Cadastrar</strong>
+          <DevFrom onSubmit={handleAddDev}/>
+        </aside>
+
+        <main>
+          <ul>
+          { devs.map(dev => <DevItem key={dev._id} dev={dev} />) }
+          </ul>
+        </main>
+      </div>
     </div>
   );
 }
